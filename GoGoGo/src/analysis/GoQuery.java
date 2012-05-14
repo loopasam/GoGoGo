19d73860d4922ba2523962786bc6bceb3068cc91
Samuel Croset <samuel.croset@gmail.com>
2012-05-14 12:12:51 +0100

    ontology reasoning implemented

--- GoGoGo/src/analysis/GoQuery.java
+++ GoGoGo/src/analysis/GoQuery.java
@@ -5,15 +5,19 @@ package analysis;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Set;
 
 import gene_ontology.GeneOntology;
 import gene_ontology.GoRelation;
 import gene_ontology.GoTerm;
 import gogogo.GoGoGoDataset;
 
+import org.semanticweb.HermiT.Reasoner;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.AddAxiom;
 import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLDataFactory;
@@ -21,8 +25,13 @@ import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
-import org.semanticweb.owlapi.util.SimpleIRIMapper;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+import org.semanticweb.owlapi.reasoner.ConsoleProgressMonitor;
+import org.semanticweb.owlapi.reasoner.NodeSet;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerConfiguration;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
 
 /**
  * @author Samuel Croset
@@ -30,23 +39,184 @@ import org.semanticweb.owlapi.util.SimpleIRIMapper;
  */
 public class GoQuery {
 
-    public static void main(String[] args) throws OWLOntologyCreationException, IOException, ClassNotFoundException {
+
+    OWLObjectProperty positivelyRegulates;
+    OWLObjectProperty negativelyRegulates;
+    OWLObjectProperty regulates;
+    OWLObjectProperty partOf;
+    OWLObjectProperty hasPart;
+    OWLOntologyManager manager;
+    OWLOntology ontology;
+    OWLDataFactory factory;
+    GeneOntology go;
+    String prefix;
+    HashMap<String, OWLObjectProperty> mapper;
+
+
+    public HashMap<String, OWLObjectProperty> getMapper() {
+	return mapper;
+    }
+    public void setMapper(HashMap<String, OWLObjectProperty> mapper) {
+	this.mapper = mapper;
+    }
+    public String getPrefix() {
+	return prefix;
+    }
+    public void setPrefix(String prefix) {
+	this.prefix = prefix;
+    }
+    public GeneOntology getGo() {
+	return go;
+    }
+    public void setGo(GeneOntology go) {
+	this.go = go;
+    }
+    public OWLOntologyManager getManager() {
+	return manager;
+    }
+    public void setManager(OWLOntologyManager manager) {
+	this.manager = manager;
+    }
+    public OWLOntology getOntology() {
+	return ontology;
+    }
+    public void setOntology(OWLOntology ontology) {
+	this.ontology = ontology;
+    }
+    public OWLDataFactory getFactory() {
+	return factory;
+    }
+    public void setFactory(OWLDataFactory factory) {
+	this.factory = factory;
+    }
+
+    public OWLObjectProperty getPositivelyRegulates() {
+	return positivelyRegulates;
+    }
+    public void setPositivelyRegulates(OWLObjectProperty positivelyRegulates) {
+	this.positivelyRegulates = positivelyRegulates;
+    }
+    public OWLObjectProperty getNegativelyRegulates() {
+	return negativelyRegulates;
+    }
+    public void setNegativelyRegulates(OWLObjectProperty negativelyRegulates) {
+	this.negativelyRegulates = negativelyRegulates;
+    }
+    public OWLObjectProperty getRegulates() {
+	return regulates;
+    }
+    public void setRegulates(OWLObjectProperty regulates) {
+	this.regulates = regulates;
+    }
+    public OWLObjectProperty getPartOf() {
+	return partOf;
+    }
+
+    public void setPartOf(OWLObjectProperty partOf) {
+	this.partOf = partOf;
+    }
+
+    public OWLObjectProperty getHasPart() {
+	return hasPart;
+    }
+    public void setHasPart(OWLObjectProperty hasPart) {
+	this.hasPart = hasPart;
+    }
+
+    public GoQuery(String path) throws IOException, ClassNotFoundException, OWLOntologyCreationException {
 
 	GoGoGoDataset data = new GoGoGoDataset("data/dataset.ser");
-	GeneOntology go = data.getGo();
+	this.setGo(data.getGo());
 
-	OWLOntologyManager man = OWLManager.createOWLOntologyManager();
-	String base = "http://org.semanticweb.restrictionexample";
+	this.setManager(OWLManager.createOWLOntologyManager());
+	this.setPrefix("http://www.gogogo.org/skeleton.owl");
+	File file = new File("data/skeleton.owl");
 
-	File file = new File("data/flamingo.owl");
+	this.setOntology(this.getManager().loadOntologyFromOntologyDocument(file));
+	this.setFactory(this.getManager().getOWLDataFactory());
 
-	OWLOntology ont = man.loadOntologyFromOntologyDocument(file);
+	HashMap<String, OWLObjectProperty> mapper = new HashMap<String, OWLObjectProperty>();
 
-	OWLDataFactory factory = man.getOWLDataFactory();
+	IRI iriPositivelyRegulates = IRI.create(this.getPrefix() + "#positively-regulates");
+	this.setPositivelyRegulates(factory.getOWLObjectProperty(iriPositivelyRegulates));
+	mapper.put("positively_regulates", this.getPositivelyRegulates());
 
-	IRI iriPositivelyRegulates = IRI.create(base + "#positively_regulates");
-	OWLObjectProperty property = factory.getOWLObjectProperty(iriPositivelyRegulates);
+	IRI iriNegativelyRegulates = IRI.create(this.getPrefix() + "#negatively-regulates");
+	this.setNegativelyRegulates(factory.getOWLObjectProperty(iriNegativelyRegulates));
+	mapper.put("negatively_regulates", this.getNegativelyRegulates());
 
+	IRI iriRegulates = IRI.create(this.getPrefix() + "#regulates");
+	this.setRegulates(factory.getOWLObjectProperty(iriRegulates));
+	mapper.put("regulates", this.getRegulates());
+
+	IRI iripartOf = IRI.create(this.getPrefix() + "#part-of");
+	this.setPartOf(factory.getOWLObjectProperty(iripartOf));
+	mapper.put("part_of", this.getPartOf());
+
+	IRI iriHasPart = IRI.create(this.getPrefix() + "#has-part");
+	this.setHasPart(factory.getOWLObjectProperty(iriHasPart));
+	mapper.put("has_part", this.getHasPart());
+
+	this.setMapper(mapper);
+
+    }
+
+    public static void main(String[] args) throws OWLOntologyCreationException, IOException, ClassNotFoundException, OWLOntologyStorageException {
+
+	System.out.println("loading data...");
+	GoQuery querier = new GoQuery("data/dataset.ser");
+
+
+	System.out.println("srating conversion...");
+	for (GoTerm term : querier.getGo().getTerms()) {
+	    OWLClass owlChildTerm = querier.getFactory().getOWLClass(IRI.create(querier.getPrefix() + "#" + term.getId()));
+	    for (GoRelation relation : term.getRelations()) {
+
+		OWLClass owlParentTerm = querier.getFactory().getOWLClass(IRI.create(querier.getPrefix() + "#" + relation.getTarget()));
+		OWLAxiom axiom = null;
+		if(relation.getType().equals("is_a")){
+		    axiom = querier.getFactory().getOWLSubClassOfAxiom(owlChildTerm, owlParentTerm);
+		}else{
+		    if(querier.getMapper().get(relation.getType()) != null){
+			OWLClassExpression relationSomeClass = querier.getFactory().getOWLObjectSomeValuesFrom(querier.getMapper().get(relation.getType()), owlParentTerm);
+			axiom = querier.getFactory().getOWLSubClassOfAxiom(owlChildTerm, relationSomeClass);
+		    }else{
+			System.err.println("Strange relation: " + relation.getType());
+		    }
+		}
+
+		if(axiom != null){
+		    AddAxiom addAxiom = new AddAxiom(querier.getOntology(), axiom);
+		    querier.getManager().applyChange(addAxiom);
+		}
+
+	    }
+	}
+
+	System.out.println("Saving...");
+	File file = new File("data/go.owl");
+	querier.getManager().saveOntology(querier.getOntology(), IRI.create(file.toURI()));
+
+	OWLReasonerFactory reasonerFactory = new Reasoner.ReasonerFactory();
+	ConsoleProgressMonitor progressMonitor = new ConsoleProgressMonitor();
+	OWLReasonerConfiguration config = new SimpleConfiguration(progressMonitor);
+	OWLReasoner reasoner = reasonerFactory.createReasoner(querier.getOntology(), config);
+	reasoner.precomputeInferences();
+	boolean consistent = reasoner.isConsistent();
+	System.out.println("Consistent: " + consistent);
+	
+	OWLClass apoptosis = querier.getFactory().getOWLClass(IRI.create(querier.getPrefix() + "#GO:0006915"));
+	
+	OWLClassExpression restrictionQuery = querier.getFactory().getOWLObjectSomeValuesFrom(querier.getRegulates(), apoptosis);
+
+	
+	NodeSet<OWLClass> subClses = reasoner.getSubClasses(restrictionQuery, false);
+	Set<OWLClass> clses = subClses.getFlattened();
+	System.out.println("Subclasses of regulates apoptosis: ");
+	for(OWLClass cls : clses) {
+	    System.out.println("    " + cls);
+	}
+	System.out.println("\n");
 
     }
 
