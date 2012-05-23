f55935a3000a6d68897d9be9db3f115caa5102a3
Samuel Croset <samuel.croset@gmail.com>
2012-05-23 11:48:14 +0100

    exploration of drugbank content distribution

---
+++ GoGoGo/src/analysis/DrugBankContent.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package analysis;
+
+import java.io.IOException;
+
+import drugbank.Drug;
+import drugbank.DrugBank;
+import drugbank.Partner;
+
+import gogogo.GoGoGoDataset;
+
+/**
+ * @author Samuel Croset
+ *
+ */
+public class DrugBankContent {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+	GoGoGoDataset data = new GoGoGoDataset("data/dataset-filtered.ser");
+	DrugBank drugBank = data.getDrugbank();
+
+	for (Partner partner : drugBank.getPartners()) {
+
+
+	}
+
+    }
+
+}
